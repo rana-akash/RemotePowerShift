f78380781b1505ceca57412f2656386116fc0d21
akr <akashrana3333@gmail.com>
2024-10-31 23:25:37 +0530

    init

---
+++ RemotePower/Controllers/CommandController.cs
@@ -0,0 +1,72 @@
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace RemotePower.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CommandController : ControllerBase
+{
+    private readonly ILogger<CommandController> _logger;
+    private readonly string connectionString = "Server=tcp:aranaa1.database.windows.net,1433;Initial Catalog=aranaa1;Persist Security Info=False;User ID=arana;Password=alaska@2";
+    public CommandController(ILogger<CommandController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetCommand")]
+    public bool Get()
+    {
+        string procedureName = "GetCommand";
+        var result = new List<bool>();
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand(procedureName, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        bool res = bool.Parse(reader[0].ToString());
+                        result.Add(res);
+                    }
+                }
+            }
+            connection.Close();
+        }
+
+        if (result.Count == 0)
+        {
+            throw new ApplicationException("Command not found");
+        }
+        return result[0];
+    }
+    
+    [HttpPost(Name = "PostCommand")]
+    public string Post(bool input)
+    {
+        string procedureName = "PostCommand";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand command = new SqlCommand(procedureName, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@command", input));
+                command.ExecuteReader();
+            }
+            connection.Close();
+        }
+
+        return "success";
+    }
+}
+
+public class Command
+{
+    public bool command;
+}
